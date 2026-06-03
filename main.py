5786b59c325be783eb01a36116c9b0d353a4c6a9
WSQS <17792626+WSQS@users.noreply.github.com>
2026-06-03 09:48:36 +0800

    feat: parse and display RSS feed titles and links

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import tomllib
 from dataclasses import dataclass
 from pathlib import Path
-from urllib.request import urlopen
-import tomllib
 from typing import Any
+from urllib.request import urlopen
+from xml.etree import ElementTree
 
 
 @dataclass
@@ -29,7 +30,27 @@ def main():
         print(f"Handle: {source.title}")
         with urlopen(source.feed_url) as response:
             xml = response.read().decode("utf-8")
-        print(xml)
+        root = ElementTree.fromstring(xml)
+        if root.tag == "rss":
+            for item in root:
+                if item.tag == "channel":
+                    for item in item:
+                        match item.tag:
+                            case "title":
+                                print(f"Title: {item.text}")
+                            case "link":
+                                print(f"Link: {item.text}")
+                            case "description":
+                                print(f"Description: {item.text}")
+                            case "item":
+                                for item in item:
+                                    match item.tag:
+                                        case "title":
+                                            print(f"Title: {item.text}")
+                                        case _:
+                                            pass
+                            case _:
+                                print(f"Unknown Handle: {item.tag}")
 
 
 if __name__ == "__main__":
